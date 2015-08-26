33124fe86f3db2202c69164f162d05c80ec639ba
Oliver Salzburg <oliver.salzburg@gmail.com>
2015-08-26 15:53:52 +0200

    [FEATURE] Initial implementation

--- test/test.js
+++ test/test.js
@@ -1,9 +1,44 @@
+// jscs:disable disallowAnonymousFunctions, requireNamedUnassignedFunctions, requireSpaceAfterKeywords
 var should = require( "should" );
 
-describe( "Main", function() {
-	describe( "Suite 1", function() {
-		it( "should do something", function( done ) {
-			setTimeout( done, 200 );
-		} );
+var exec = require( "../lib" );
+
+describe( "fm-exec", function() {
+	it( "should execute something", function( done ) {
+		exec( "echo" )
+			.then( done )
+			.catch( done );
+	} );
+
+	it( "should fail for invalid binaries", function( done ) {
+		exec( "SHOULD_NOT_EXIST" )
+			.then( function() {
+				done( new Error( "exec should fail for nonexistent binaries" ) );
+			} )
+			.catch( function( error ) {
+				done();
+			} );
+	} );
+
+	it( "should not fail for invalid binaries, when ignoreFailure is set", function( done ) {
+		exec( "SHOULD_NOT_EXIST", [], {
+			ignoreFailure : true
+		} )
+			.then( function( result ) {
+				result.should.be.an.instanceOf( Error );
+				done();
+			} )
+			.catch( done );
+	} );
+
+	it( "should buffer output if instructed", function( done ) {
+		exec( "echo", [ "lol" ], {
+			buffer : true
+		} )
+			.then( function( result ) {
+				result.should.have.property( "stdout" ).with.type( "string" ).and.equal( "lol" );
+				done();
+			} )
+			.catch( done );
 	} );
 } );
