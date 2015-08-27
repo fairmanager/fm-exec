e30f2de66b131daf3b63ac01435565229bacf298
Oliver Salzburg <oliver.salzburg@gmail.com>
2015-08-27 14:09:16 +0200

    [FEATURE] Added wrapper for execution through the shell

--- test/test.js
+++ test/test.js
@@ -4,41 +4,91 @@ var should = require( "should" );
 var exec = require( "../lib" );
 
 describe( "fm-exec", function() {
-	it( "should execute something", function( done ) {
-		exec( "echo" )
-			.then( done )
-			.catch( done );
+	describe( "spawn", function() {
+		it( "should execute something", function( done ) {
+			exec( "echo" )
+				.then( done )
+				.catch( done );
+		} );
+
+		it( "should fail for invalid binaries", function( done ) {
+			exec( "SHOULD_NOT_EXIST" )
+				.then( function() {
+					done( new Error( "exec should fail for nonexistent binaries" ) );
+				} )
+				.catch( function( error ) {
+					done();
+				} );
+		} );
+
+		it( "should not fail for invalid binaries, when ignoreFailure is set", function( done ) {
+			exec( "SHOULD_NOT_EXIST", [], {
+				ignoreFailure : true
+			} )
+				.then( function( result ) {
+					result.should.be.an.instanceOf( Error );
+					done();
+				} )
+				.catch( done );
+		} );
+
+		it( "should buffer output if instructed", function( done ) {
+			exec( "echo", [ "lol" ], {
+				buffer : true
+			} )
+				.then( function( result ) {
+					result.should.have.type( "string" ).and.equal( "lol" );
+					done();
+				} )
+				.catch( done );
+		} );
 	} );
 
-	it( "should fail for invalid binaries", function( done ) {
-		exec( "SHOULD_NOT_EXIST" )
-			.then( function() {
-				done( new Error( "exec should fail for nonexistent binaries" ) );
-			} )
-			.catch( function( error ) {
-				done();
-			} );
-	} );
+	describe( "shell", function() {
+		it( "should execute something", function( done ) {
+			exec.shell( "echo" )
+				.then( done )
+				.catch( done );
+		} );
 
-	it( "should not fail for invalid binaries, when ignoreFailure is set", function( done ) {
-		exec( "SHOULD_NOT_EXIST", [], {
-			ignoreFailure : true
-		} )
-			.then( function( result ) {
-				result.should.be.an.instanceOf( Error );
-				done();
-			} )
-			.catch( done );
-	} );
+		it( "should fail for invalid binaries", function( done ) {
+			exec.shell( "SHOULD_NOT_EXIST" )
+				.then( function() {
+					done( new Error( "exec should fail for nonexistent binaries" ) );
+				} )
+				.catch( function( error ) {
+					done();
+				} );
+		} );
 
-	it( "should buffer output if instructed", function( done ) {
-		exec( "echo", [ "lol" ], {
-			buffer : true
-		} )
-			.then( function( result ) {
-				result.should.have.property( "stdout" ).with.type( "string" ).and.equal( "lol" );
-				done();
+		it( "should not fail for invalid binaries, when ignoreFailure is set", function( done ) {
+			exec.shell( "SHOULD_NOT_EXIST", [], {
+				ignoreFailure : true
 			} )
-			.catch( done );
+				.then( function( result ) {
+					result.should.be.an.instanceOf( Error );
+					done();
+				} )
+				.catch( done );
+		} );
+
+		it( "should buffer output if instructed", function( done ) {
+			exec.shell( "echo", [ "lol" ], {
+				buffer : true
+			} )
+				.then( function( result ) {
+					result.should.have.type( "string" ).and.equal( "lol" );
+					done();
+				} )
+				.catch( done );
+		} );
+
+		it( "should accept pipes", function( done ) {
+			exec.shell( "echo foo | echo" )
+				.then( function() {
+					done();
+				} )
+				.catch( done );
+		} );
 	} );
 } );
